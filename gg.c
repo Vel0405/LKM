5e82803c5d6b1da79e2041a8d9d092ba805bf8bc
Vel0405 <59429580+Vel0405@users.noreply.github.com>
2020-04-20 16:32:49 +0530

    Add files via upload

--- gg.c
+++ gg.c
@@ -10,33 +10,50 @@ static struct task_struct *ts;
 int check_battery(void)
 {
     // Create variables
-    struct file *f;
-    char buf[128];
-    mm_segment_t fs;
-    int i;
+    	struct file *f,*g,*h;
+    	char buf[128];
+	int bval=10,i,b1val=0;
+    	mm_segment_t fs;
+	ssize_t rk=0,rk1=0;
+	size_t len = sizeof(bval);
+	size_t len1=sizeof(b1val);
     // Init the buffer with 0
-    for(i=0;i<128;i++)
-        buf[i] = 0;
+    	for(i=0;i<128;i++)
+	{
+		buf[i] = 0;
+	}
     // To see in /var/log/messages that the module is operating
-    printk(KERN_INFO "My module is loaded\n");
-    // I am using Fedora and for the test I have chosen following file
-    // Obviously it is much smaller than the 128 bytes, but hell with it =)
-    f = filp_open("/sys/class/power_supply/BAT1/capacity", O_RDONLY, 0);
-    if(f == NULL)
-        printk(KERN_ALERT "filp_open error!!.\n");
-    else{
-        // Get current segment descriptor
-        fs = get_fs();
-        // Set segment descriptor associated to kernel space
-        set_fs(KERNEL_DS);
-        // Read the file
-        f->f_op->read(f, buf, 128, &f->f_pos);
-        // Restore segment descriptor
-        set_fs(fs);
-        // See what we read from file
-	printk(KERN_INFO " buf:%s\n",buf[0]);
-    }
+    	printk(KERN_INFO "My module is loaded\n");
 
+	f = filp_open("/sys/class/power_supply/BAT1/capacity", O_RDONLY, 0);
+    	if(f == NULL)
+        	printk(KERN_ALERT "filp_open error!!.\n");
+    	else{
+        	// Get current segment descriptor
+        	fs = get_fs();
+        	// Set segment descriptor associated to kernel space
+        	set_fs(KERNEL_DS);
+        	// Read the file
+        	f->f_op->read(f, buf, 128, &f->f_pos);
+        	// Restore segment descriptor
+        	set_fs(fs);
+		int buf1;
+		if(kstrtoint(buf, 10, &buf1) != 0)
+        	{
+			return -1;
+		}
+		char bright[20], bluet[10]; /* should be large enough */
+		if(buf1<=98)
+		{
+			g=filp_open("/sys/class/backlight/nvidia_0/brightness",O_RDWR,0);
+    			len = scnprintf(bright, sizeof(bright), "%d\n", bval);
+    			rk = kernel_write(g,bright,len,&g->f_pos);
+			h=filp_open("/sys/class/bluetooth/hci0/rfkill0/state",O_RDWR,0);
+			len1 = scnprintf(bluet, sizeof(bluet), "%d\n", b1val);
+			rk1 = kernel_write(h,bluet,len1,&h->f_pos);
+	 		printk(KERN_ALERT "battery is low  int%d",buf1);
+		}
+    }
     filp_close(f,NULL);
     return 0;
 }
